3cd3ec605499f7012c8da80a16e6c35601f41ffd
Leonardo Torres Briones <dleonardotb2001@gmail.com>
2020-12-28 12:52:38 -0500

    core: Modelo de esquemas en mongoose

---
+++ Desarrollo/Artemis/Codificación/BackEnd/models/user.js
@@ -0,0 +1,47 @@
+const mongoose=require('mongoose')
+const uniqueValidator=require('mongoose-unique-validator')
+
+let Schema=mongoose.Schema
+
+let userSchema=new Schema({
+    artistic_name:{
+        type: String,
+        unique: true,
+        required: [true,'Artistic name is required']
+    },
+    nick_name:{
+        type: String,
+        unique: true,
+        required: [true,'Nick name is required']
+    },
+    email:{
+        type: String,
+        unique: true,
+        required:[true,'Email is required']
+    },
+    password:{
+        type: String,
+        required:[true,'Password is required']
+    },
+    profile_img:{
+        type: String
+    },
+    followers_id:[{
+        type: String
+    }],
+    folders_id:[{
+        type: String
+    }],
+    signed_google:{
+        type: Boolean,
+        default: false
+    },
+    signed_facebook:{
+        type: Boolean,
+        default: false
+    }
+})
+
+userSchema.plugin(uniqueValidator,{message:'{PATH} has to be unique'});
+
+module.exports=mongoose.model('User',userSchema);
