631b6393097870ce00b2b0db4b9b02cd9c894b8f
Sebastian Vargas <vargaspizango1@gmail.com>
2020-12-28 15:43:27 -0500

    chore: Primeros componentes compartidos

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/app.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInFormComponent } from './welcome/sign-in-form/sign-in-form.component';
 import { ApiFormComponent } from './welcome/api-form/api-form.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInFormComponent,
-    ApiFormComponent
+    ApiFormComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
