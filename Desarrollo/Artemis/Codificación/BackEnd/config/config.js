767c2499ad37069c8492c44bc99ada9756ed4103
Maurck <mau9977@gmail.com>
2020-12-28 13:25:26 -0500

    fix: coneccion a bd

--- Desarrollo/Artemis/Codificación/BackEnd/config/config.js
+++ Desarrollo/Artemis/Codificación/BackEnd/config/config.js
@@ -1,13 +1,9 @@
 const mongoose = require('mongoose')
 
 process.env.PORT=process.env.PORT || 3000
-process.env.MONGO_URL=process.env.MONGO_URL || 'mongodb+srv://Mauricio:1234@clusterartemis.ofy5d.mongodb.net/Artemis?retryWrites=true&w=majority'
+process.env.MONGO_URL= 'mongodb+srv://Mauricio:1234@clusterartemis.ofy5d.mongodb.net/Artemis?retryWrites=true&w=majority'
 
-let urlDataBase = process.env.MONGO_URL
-
-process.env.URLDB=urlDataBase
-
-mongoose.connect(process.env.URLDB,{ 
+mongoose.connect(process.env.MONGO_URL,{ 
     useNewUrlParser: true, 
     useUnifiedTopology: true,
     useCreateIndex: true
