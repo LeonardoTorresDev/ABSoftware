238d4ffc6bfae221bb0cd44614b096eebc676a61
Sebastian Vargas <vargaspizango1@gmail.com>
2020-12-31 01:32:16 -0500

    feat: Se agrega primer modelo de form de final de landing page
    Se agrega la parte esencial del formulario del final de la página de bienvenida. No obstante, faltan agregar los estilos principales. Asimismo, aún se tiene añadida una característica de validación de desarrollo que se ha de eliminar cuando se acabe de implementar el módulo correspondiente

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/components/form-end/form-end.component.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/components/form-end/form-end.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-end',
   templateUrl: './form-end.component.html',
-  styleUrls: ['./form-end.component.scss']
+  styleUrls: ['./form-end.component.scss'],
 })
 export class FormEndComponent implements OnInit {
+  forma: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.crearFormulario();
   }
 
+  ngOnInit(): void {}
+
+  crearFormulario() {
+    this.forma = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
+      apellido: ['', Validators.required],
+      nickname: ['', Validators.required],
+      termsAndConds: [false, Validators.requiredTrue],
+      // [Valor por defecto, valores síncronos, validadores asíncronos]
+    });
+  }
+
+  noValido(attr: string) {
+    return this.forma.get(attr).invalid && this.forma.get(attr).touched;
+  }
+
+  termsCheck() {
+    const valor = this.forma.get('termsAndConds');
+
+    if (valor.value === false) {
+      return valor.setValue(true);
+    }
+    return valor.setValue(false);
+  }
+
+  guardar() {
+    console.log(this.forma);
+
+    if (this.forma.invalid) {
+      return Object.values(this.forma.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+    }
+  }
 }
--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/landing-page.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/landing-page.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormStartComponent } from './components/form-start/form-start.component';
 import { FormEndComponent } from './components/form-end/form-end.component';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FormStartComponent, FormEndComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, BrowserModule, ReactiveFormsModule],
+  exports: [FormStartComponent, FormEndComponent],
 })
-export class LandingPageModule { }
+export class LandingPageModule {}
