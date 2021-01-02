60584aa908885195f68f9ab383aaaca92c5e111a
Sebastian Vargas <vargaspizango1@gmail.com>
2021-01-02 13:54:06 -0500

    perf: Sección estática de landing page completada
    Se añadieron los elementos estáticos de la landing page, quedando solo pulir algunos detalles en la lógica de formularios.

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/landing-page.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/landing-page/landing-page.module.ts
@@ -4,10 +4,12 @@ import { FormStartComponent } from './components/form-start/form-start.component
 import { FormEndComponent } from './components/form-end/form-end.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InfoLandingComponent } from './components/info-landing/info-landing.component';
+import { NavbarLandingComponent } from './components/navbar-landing/navbar-landing.component';
 
 @NgModule({
-  declarations: [FormStartComponent, FormEndComponent],
+  declarations: [FormStartComponent, FormEndComponent, InfoLandingComponent, NavbarLandingComponent],
   imports: [CommonModule, BrowserModule, ReactiveFormsModule],
-  exports: [FormStartComponent, FormEndComponent],
+  exports: [FormStartComponent, FormEndComponent, InfoLandingComponent, NavbarLandingComponent],
 })
 export class LandingPageModule {}
