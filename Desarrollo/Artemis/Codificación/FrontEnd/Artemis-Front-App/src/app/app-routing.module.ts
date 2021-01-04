df98e8e5e331f53bb22dc66f370b617acbc11992
Sebastian Vargas <vargaspizango1@gmail.com>
2021-01-04 15:40:38 -0500

    fix: Arreglos del enrutamiento
    Se mejoraron ciertos aspectos del enrutamiento.

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/app-routing.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/app-routing.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingPageComponent } from './modules/landing-page/landing-page.component';
 import { HomeComponent } from './modules/home/home.component';
+import { LandingPageRoutingModule } from './modules/landing-page/landing-page-routing.module';
 
 const routes: Routes = [
   { path: 'welcome', component: LandingPageComponent },
   { path: 'home', component: HomeComponent }, // Se necesitará de un guard para que el usuario no entre a home si no está logueado
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' }, //Por mientras, redirigirá a la landing page: se necesita de una página 404
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' }, //Por mientras, redirigirá a la landing page: se necesita de una página 404
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), LandingPageRoutingModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/sign/sign.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/sign/sign.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SignInComponent } from './components/sign-in/sign-in.component';
 import { FormDataComponent } from './components/form-data/form-data.component';
 import { OtherNetworksComponent } from './components/other-networks/other-networks.component';
 import { WelcomingComponent } from './components/welcoming/welcoming.component';
-import { RouterModule } from '@angular/router';
 import { NavbarSignComponent } from './components/navbar-sign/navbar-sign.component';
 
 @NgModule({
