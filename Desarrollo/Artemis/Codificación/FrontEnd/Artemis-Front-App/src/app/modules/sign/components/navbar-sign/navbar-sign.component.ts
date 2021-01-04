e8740dba55b269de93e009ffd7ce30a901af80b3
Sebastian Vargas <vargaspizango1@gmail.com>
2021-01-04 00:05:51 -0500

    code: Implementación de las vistas sign in y sign up
    Se implementó las vistas para registro e inicio de sesión, aplicando el layout del mockup.

---
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/sign/components/navbar-sign/navbar-sign.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-sign',
+  templateUrl: './navbar-sign.component.html',
+  styleUrls: ['./navbar-sign.component.scss']
+})
+export class NavbarSignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
