a6535346c9c378d9ca7485b0b6771e09801b1d41
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-01-06 16:33:18 -0500

    chore: Creación de los componentes del módulo Home, tercer módulo a trabajar

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/home/home.module.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/home/home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { PublicacionComponent } from './components/publicacion/publicacion.component';
 import { HomeComponent } from './home.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [PublicacionComponent, HomeComponent],
+  declarations: [PublicacionComponent, HomeComponent, SidebarComponent],
   imports: [CommonModule, HomeRoutingModule],
 })
 export class HomeModule {}
