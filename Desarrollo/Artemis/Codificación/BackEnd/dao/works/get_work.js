11e19af002760c38b52a1d90494693e9cbbbf096
Maurck <mau9977@gmail.com>
2021-01-06 10:26:03 -0500

    fix: Configuracion CORS actualizada
    CORS actualizado para el servidor frontend

--- Desarrollo/Artemis/Codificación/BackEnd/dao/works/get_work.js
+++ Desarrollo/Artemis/Codificación/BackEnd/dao/works/get_work.js
@@ -18,7 +18,14 @@ function get_work(req, res){
 
             if(folder==null){ return custom_error_response(400, res, "Folder no encontrado") }
 
-            
+            Work.findOne({name: req.query.folder_name, folder: folder._id})
+            .exec(function (err, work){
+                if(err){ return error_response(400, res, err) }
+
+                if(work==null){ return custom_error_response(400, res, "Obra no encontrada") }
+
+                res.send(work)
+            })
         })
     })
 }
