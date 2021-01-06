f5b1f36cfe9e6e4034bf4a7c7eec88cda401f2aa
Maurck <mau9977@gmail.com>
2021-01-06 14:12:17 -0500

    code: Uso de la libreria CORS
    Librería CORS usada para configurar el CORS del servidor frontend

--- Desarrollo/Artemis/Codificación/BackEnd/config/cors_config.js
+++ Desarrollo/Artemis/Codificación/BackEnd/config/cors_config.js
@@ -1,20 +1,13 @@
+var cors = require('cors');
+
 function cors_config(app)
 {
-    app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-    
-        // authorized headers for preflight requests
-        // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-        next();
-    
-        app.options('http://localhost:4200', (req, res) => {
-            // allowed XHR methods  
-            res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
-            res.header('Access-Control-Allow-Credentials', true);
-            res.send();
-        });
-    });
+    app.use(cors({
+        origin: 'http://localhost:4200',
+        methods: ['GET', 'PATCH', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+        credentials: true
+    }));
 }
 
 module.exports={cors_config}
