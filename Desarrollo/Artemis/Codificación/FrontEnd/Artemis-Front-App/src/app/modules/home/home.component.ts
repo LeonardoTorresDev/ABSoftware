2830d1e6cff801af2df0bcccb614601c4ec29df2
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2021-01-08 13:36:31 -0500

    code: Finalización de los componentes del módulo Home y creación del servicio LoadPost

--- Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/home/home.component.ts
+++ Desarrollo/Artemis/Codificación/FrontEnd/Artemis-Front-App/src/app/modules/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { LoadPostsService } from '../../shared/services/load-posts/load-posts.service'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  providers: [LoadPostsService]
 })
 export class HomeComponent {
-  constructor() {}
 
-  ngOnInit(): void {}
+  postsLoaded: any[]
+
+  constructor(private loadPosts: LoadPostsService) {}
+
+  ngOnInit(): void {
+    this.postsLoaded = this.loadPosts.getPosts()
+  }
+
 }
